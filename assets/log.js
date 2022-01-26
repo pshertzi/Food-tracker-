23914949f0b45a60c9f325e989826d868ff70a54
Paul Shertzinger <pshertzi@gmail.com>
2022-01-26 16:08:11 -0500

    btn navigation

---
+++ assets/log.js
@@ -0,0 +1,14 @@
+document.getElementById("recipes-nutrition").onclick = function () {
+    location.href = "./recipe.html";
+
+};
+
+document.getElementById("location").onclick = function () {
+    location.href = "./location.html";
+
+};
+
+document.getElementById("home-btn").onclick = function () {
+    location.href = "./index.html";
+
+};
---
+++ assets/script.js
@@ -0,0 +1,21 @@
+
+document.getElementById("recipes-nutrition").onclick = function () {
+    location.href = "./recipe.html";
+
+};
+
+document.getElementById("food-log").onclick = function () {
+    location.href = "./foodlog.html";
+
+};
+
+document.getElementById("location").onclick = function () {
+    location.href = "./location.html";
+
+};
+
+document.getElementById("home-btn").onclick = function () {
+    location.href = "./index.html";
+
+};
+
