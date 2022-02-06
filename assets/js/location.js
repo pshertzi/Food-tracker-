c5ec4576dd5a186c296b58707f50307a3c1e2543
EsteMaeLeo <leonel_morales20@hotmail.com>
2022-02-05 19:34:38 -0600

    add message error

--- assets/js/location.js
+++ assets/js/location.js
@@ -1,6 +1,9 @@
+var error = document.querySelector('.modal-error')
+
 var displayMarket = function (data, location) {
 
-    //data.resourceSets[0].resources.length
+    $('#list-market').append('<div class = "message-body">');
+    
     if (data) {
         switch (location) {
             case "Kroger": $('#list-market').append('<li>' + location + ' near to your location</li>');
@@ -10,7 +13,7 @@ var displayMarket = function (data, location) {
             case "Walmart": $('#list-market').append('<li>' + location + ' near to your location</li>');
                 break;
         }
-
+        $('#list-market').append('<div class = "message-body">');
         for (var i = 0; i < data.resourceSets.length; i++) {
             for (var j = 0; j < data.resourceSets[i].resources.length; j++) {
                 $('#list-market').append('<li>' + data.resourceSets[i].resources[j].Address.formattedAddress + '</li>');
@@ -24,15 +27,21 @@ var getKroger = function () {
     ;
     var apiUrl = "https://dev.virtualearth.net/REST/v1/LocalSearch/?query=kroger&key=ArrXVS9tBqxMsaC7kak_u6KXH9cBWlwJX7TwwSmscPhqu49ammtN20q2Vh45JkVQ"
     fetch(apiUrl)
-        .then(response => {
+    .then(function (response) {
+        if (response.ok) {
             response.json().then(function (data) {
-                displayMarket(data, "Kroger");
+                displayMarket(data, "Walmart");
+
 
             });
-        })
-        .catch(err => {
-            console.error(err);
-        });
+        } else {
+            showError(error);
+        }
+    })
+    .catch(function (error) {
+        showError(error);
+    });
+
 }
 
 //Call API BING to get Publix near by IP 
@@ -40,33 +49,56 @@ var getPublix = function () {
     ;
     var apiUrl = "https://dev.virtualearth.net/REST/v1/LocalSearch/?query=publix&key=ArrXVS9tBqxMsaC7kak_u6KXH9cBWlwJX7TwwSmscPhqu49ammtN20q2Vh45JkVQ"
     fetch(apiUrl)
-        .then(response => {
+    .then(function (response) {
+        if (response.ok) {
             response.json().then(function (data) {
-                displayMarket(data, "Publix");
+                displayMarket(data, "Walmart");
+
 
             });
-        })
-        .catch(err => {
-            console.error(err);
-        });
+        } else {
+            showError(error);
+        }
+    })
+    .catch(function (error) {
+        showError(error);
+    });
+
 }
 
+var showError = function (error) {
+    $("#error-msg").append('<p>' + error)
+    $("#error-message").addClass("is-active");
+}
 
 //Call API BING to get Walmart near by IP 
 var getWalmart = function () {
     ;
     var apiUrl = "https://dev.virtualearth.net/REST/v1/LocalSearch/?query=walmart&key=ArrXVS9tBqxMsaC7kak_u6KXH9cBWlwJX7TwwSmscPhqu49ammtN20q2Vh45JkVQ"
     fetch(apiUrl)
-        .then(response => {
-            response.json().then(function (data) {
-                displayMarket(data, "Walmart");
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    displayMarket(data, "Walmart");
 
-            });
+
+                });
+            } else {
+                showError(error);
+            }
         })
-        .catch(err => {
-            console.error(err);
+        .catch(function (error) {
+            showError(error);
         });
+
 }
+
+var closeError = function (event) {
+
+    $("#error-message").removeClass("is-active");
+}
+
+error.addEventListener("click", closeError);
 
 $('#list-market').empty();
 getKroger();
